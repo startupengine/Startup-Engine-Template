44df1f6c5d4720196a422107b595447fa7437bbb
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-10-22 14:20:13 -0700

    Link

--- pages/search/scripts.blade.php
+++ pages/search/scripts.blade.php
@@ -4,7 +4,7 @@
     var input = url.searchParams.get("s");
     Vue.component('todo-item', {
         props: ['todo'],
-        template: '<div class="col-md-6" style="float:left;"><div class="card"><div class="card-body"><h4 class="card-title">{{ todo.title }}</h4><p class="card-text">{{ todo.meta_description }}</p><a href="{{ todo.slug }}" class="btn btn-info pull-right" style="margin-bottom:25px;">Read More</a></div></div></div>'
+        template: '<div class="col-md-6" style="float:left;"><a class="card-link" v-bind:href="todo.slug"><div class="card"><div class="card-body"><h4 class="card-title">{{ todo.title }}</h4><p class="card-text">{{ todo.meta_description }}</p><a class="btn btn-info pull-right" style="margin-bottom:25px;">Read More</a></div></div></a></div>'
     });
     var app7 = new Vue({
         el: '#articles',
