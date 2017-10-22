fb243c40a712d534e62bd6646fb924a09b0de2f2
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-10-22 12:50:47 -0700

    Help Header

---
+++ pages/help/header.blade.php
@@ -0,0 +1,18 @@
+<body class="index-page sidebar-collapse">
+<div id="startup_engine_nav_container"></div>
+<div class="wrapper">
+    <div class="page-header page-header-small clear-filter" filter-color="black">
+        <div class="page-header-image" data-parallax="true" style="background-color:#3a92f1;">
+        </div>
+        <div class="container">
+            <div class="content-center" style="top:200px !important;">
+                <h1 class="title text-center">How can we help?</h1>
+                <div class="form-group" style="padding:0px 15%;">
+                    <form method="get" enctype="multipart/form-data" action="/search">
+                        <input type="text" id="s" name="s" class="form-control" placeholder="Search for..." aria-label="Search for...">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
