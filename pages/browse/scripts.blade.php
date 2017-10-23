4aa8533ffc72f65f6a1db9f1f8ca460012534a3a
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-10-22 17:02:35 -0700

    Escaping Vue

--- pages/browse/scripts.blade.php
+++ pages/browse/scripts.blade.php
@@ -5,11 +5,11 @@
     console.log(input);
     Vue.component('todo-item', {
         props: ['todo'],
-        template: '<div class="col-sm-4"><a class="card-link" v-bind:href="todo.slug"><div class="card"><img class="card-img-top" v-bind:src="todo.image"><div class="card-body"><h4 class="card-title" align="center">{{ todo.title }}</h4><p class="card-text">{{ todo.meta_description }}</p></div><div class="card-footer" align="center">Read More</div></div></a></div>'
+        template: '<div class="col-sm-4"><a class="card-link" v-bind:href="todo.slug"><div class="card"><img class="card-img-top" v-bind:src="todo.image"><div class="card-body"><h4 class="card-title" align="center">@{{ todo.title }}</h4><p class="card-text">@{{ todo.meta_description }}</p></div><div class="card-footer" align="center">Read More</div></div></a></div>'
     });
     Vue.component('category', {
         props: ['category'],
-        template: '<div><h1 class="title">{{ category.name }}</h1><h4 class="title">{{ category.description }}</h4></div>'
+        template: '<div><h1 class="title">@{{ category.name }}</h1><h4 class="title">@{{ category.description }}</h4></div>'
     });
     var heading = new Vue({
         el: '#heading',
