149b9622453d36fb1a2ea7107697ae60fe6c9bae
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-10-23 16:06:39 -0700

    Products page

--- pages/products/body.blade.php
+++ pages/products/body.blade.php
@@ -2,32 +2,24 @@
 <div id="startup_engine_nav_container"></div>
 <div class="wrapper">
     <div class="main" >
-        <div class="section section-examples bg-gradient-orange">
+        <div class="section section-examples bg-gradient-blue" style="min-height:100vh;">
             <div class="space-50"></div>
             <div class="container text-center">
                 <div class="row justify-content-md-center" id="screenshots">
                     <div class="col-md-12" style="margin-bottom:25px;margin-top:25px;">
                         <h2 class="title" style="width:100%;color:#fff;">Tools for the Modern Startup.</h2>
-                        <h5 class="description" style="text-align:center;color:#fff;font-weight:300;">Startup Engine CMS</h5>
                     </div>
-                    <div class="col-lg-4">
-                        <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/docs/screenshots/bH2w9VVLi3Hmwal2ydbV0Cjc0gAelqrydpe8SE2u.png" alt="Laravel" class="img-raised rounded" style="max-width:100px;">
-                        <a href="https://heroku.com/deploy?template=https://github.com/luckyrabbitllc/StartupEngine" class="btn btn-simple btn-neutral btn-round " target="_blank">Learn More &nbsp; <i class="now-ui-icons arrows-1_cloud-upload-94"></i></a>
+                    <div class="col-lg-6" style="margin-bottom:25px;">
+                        <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/docs/screenshots/bH2w9VVLi3Hmwal2ydbV0Cjc0gAelqrydpe8SE2u.png" alt="Laravel" class="img-raised rounded">
+                        <h4 style="margin-top:0px;">Startup Engine CMS</h4>
+                        <p>An open-source content management app designed for Startups.</p>
+                        <a href="/cms" class="btn btn-simple btn-neutral btn-round">Learn More</a>
                     </div>
-                </div>
-            </div>
-        </div>
-        <div class="section section-examples bg-gradient-orange">
-            <div class="space-50"></div>
-            <div class="container text-center">
-                <div class="row justify-content-md-center" id="screenshots">
-                    <div class="col-md-12" style="margin-bottom:25px;margin-top:25px;">
-                        <h2 class="title" style="width:100%;color:#fff;">PsychoAnalytics</h2>
-                        <h5 class="description" style="text-align:center;color:#fff;font-weight:300;">Psycho-analyze your customers, competitors, and content.</h5>
-                    </div>
-                    <div class="col-lg-4">
-                        <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/pages/products/wrZ4y5fpWMQPkiJ0SaY4wxh5siQJHiWwzGufIHoy.png" alt="Laravel" class="img-raised rounded" style="max-width:100px;">
-                        <a href="https://heroku.com/deploy?template=https://github.com/luckyrabbitllc/StartupEngine" class="btn btn-simple btn-neutral btn-round " target="_blank">Learn More &nbsp; <i class="now-ui-icons arrows-1_cloud-upload-94"></i></a>
+                    <div class="col-lg-6" style="margin-bottom:25px;">
+                        <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/pages/products/ejinjFr2xPKaq27lB4QCRkDHdw45ayHjYLW4YMgM.jpeg" alt="Customer Psychology" class="img-raised rounded" >
+                        <h4 style="margin-top:0px;">PsychoAnalytics</h4>
+                        <p>Psycho-analyze your customers, competitors, and content.</p>
+                        <a href="https://www.psychoanalytics.io" class="btn btn-simple btn-neutral btn-round " target="_blank">Learn More</a>
                     </div>
                 </div>
             </div>
