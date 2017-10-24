6aecbcbfd08d6239e0756fcbce321e6387acdb09
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-10-23 17:19:01 -0700

    Products page

--- pages/products/body.blade.php
+++ pages/products/body.blade.php
@@ -10,13 +10,13 @@
                         <h2 class="title" style="width:100%;color:#fff;">Tools for the Modern Startup.</h2>
                     </div>
                     <div class="col-lg-6" style="margin-bottom:25px;">
-                        <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/docs/screenshots/bH2w9VVLi3Hmwal2ydbV0Cjc0gAelqrydpe8SE2u.png" alt="Laravel" class="img-raised rounded">
+                        <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/pages/products/hiWJfnFEo9yafHYHJby55zXtnjbT2fsQ89Id6xE6.jpeg" alt="Startup Engine CMS" class="img-raised rounded">
                         <h4 style="margin-top:0px;">Startup Engine CMS</h4>
                         <p>An open-source content management app designed for Startups.</p>
                         <a href="/cms" class="btn btn-simple btn-neutral btn-round">Learn More</a>
                     </div>
                     <div class="col-lg-6" style="margin-bottom:25px;">
-                        <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/pages/products/ejinjFr2xPKaq27lB4QCRkDHdw45ayHjYLW4YMgM.jpeg" alt="Customer Psychology" class="img-raised rounded" >
+                        <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/pages/products/ejinjFr2xPKaq27lB4QCRkDHdw45ayHjYLW4YMgM.jpeg" alt="PsychoAnalytics" class="img-raised rounded" >
                         <h4 style="margin-top:0px;">PsychoAnalytics</h4>
                         <p>Psycho-analyze your customers, competitors, and content.</p>
                         <a href="https://www.psychoanalytics.io" class="btn btn-simple btn-neutral btn-round " target="_blank">Learn More</a>
