60ec9cdf07e0cd876c6117a97a480eae24780c03
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-10-24 20:40:00 -0700

    Swapped image

--- pages/products/body.blade.php
+++ pages/products/body.blade.php
@@ -1,27 +1,27 @@
 <body class="index-page sidebar-collapse">
-<div id="startup_engine_nav_container"></div>
-<div class="wrapper">
-    <div class="main" >
-        <div class="section section-examples bg-gradient-blue" style="min-height:100vh;">
-            <div class="space-50"></div>
-            <div class="container text-center">
-                <div class="row justify-content-md-center" id="screenshots">
-                    <div class="col-md-12" style="margin-bottom:25px;margin-top:25px;">
-                        <h2 class="title" style="width:100%;color:#fff;">Tools for the Modern Startup.</h2>
-                    </div>
-                    <div class="col-lg-6" style="margin-bottom:25px;">
-                        <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/pages/products/hiWJfnFEo9yafHYHJby55zXtnjbT2fsQ89Id6xE6.jpeg" alt="Startup Engine CMS" class="img-raised rounded">
-                        <p>An open-source content management app designed for Startups.</p>
-                        <a href="/cms" class="btn btn-simple btn-neutral btn-round">Learn More</a>
-                    </div>
-                    <div class="col-lg-6" style="margin-bottom:25px;">
-                        <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/pages/products/ejinjFr2xPKaq27lB4QCRkDHdw45ayHjYLW4YMgM.jpeg" alt="PsychoAnalytics" class="img-raised rounded" >
-                        <p>Psycho-analyze your customers, competitors, and content.</p>
-                        <a href="https://www.psychoanalytics.io" class="btn btn-simple btn-neutral btn-round " target="_blank">Learn More</a>
+    <div id="startup_engine_nav_container"></div>
+    <div class="wrapper">
+        <div class="main" >
+            <div class="section section-examples bg-gradient-blue" style="min-height:100vh;">
+                <div class="space-50"></div>
+                <div class="container text-center">
+                    <div class="row justify-content-md-center" id="screenshots">
+                        <div class="col-md-12" style="margin-bottom:25px;margin-top:25px;">
+                            <h2 class="title" style="width:100%;color:#fff;">Tools for the Modern Startup.</h2>
+                        </div>
+                        <div class="col-lg-6" style="margin-bottom:25px;">
+                            <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/pages/products/wDtDs4wlcXUsoRylE1PkinplrNO3PzRM0rWQj6tV.jpeg" alt="Startup Engine CMS" class="img-raised rounded">
+                            <p>An open-source content marketing app for Startups.</p>
+                            <a href="/cms" class="btn btn-simple btn-neutral btn-round">Learn More</a>
+                        </div>
+                        <div class="col-lg-6" style="margin-bottom:25px;">
+                            <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/pages/products/ejinjFr2xPKaq27lB4QCRkDHdw45ayHjYLW4YMgM.jpeg" alt="PsychoAnalytics" class="img-raised rounded" >
+                            <p>Psycho-analyze your customers, competitors, and content.</p>
+                            <a href="https://www.psychoanalytics.io" class="btn btn-simple btn-neutral btn-round " target="_blank">Learn More</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </body>
