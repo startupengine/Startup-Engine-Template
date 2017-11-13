a64f9fdea86b93fdcc2ebf6dd7feee903a318d4d
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-11-12 18:13:02 -0800

    Getting started page

---
+++ pages/getting-started/body.blade.php
@@ -0,0 +1,61 @@
+<body class="index-page sidebar-collapse">
+<div id="startup_engine_nav_container"></div>
+<div class="wrapper">
+    <div class="main" >
+        <div class="section section-examples bg-gradient-orange">
+            <div class="space-50"></div>
+            <div class="container text-center">
+                <div class="row justify-content-md-center" id="screenshots">
+                    <div class="col-md-12" style="margin-bottom:25px;margin-top:25px;">
+                        <h2 class="title" style="width:100%;color:#fff;">Getting Started with Startup Engine CMS</h2>
+                        <h5 class="description" style="text-align:center;color:#fff;font-weight:300;"><strong>Step 1:</strong> deploy to Heroku.</h5>
+                    </div>
+                    <div class="col-lg-4">
+                        <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/pages/getting-started/4kkXRkyQtW7yYck5rycTpe3XMzRki4RL34xDdFS4.jpeg" alt="Deploying to Heroku" class="img-raised">
+                        <a href="https://heroku.com/deploy?template=https://github.com/luckyrabbitllc/StartupEngine" class="btn btn-simple btn-neutral btn-round " target="_blank">Click to deploy to Heroku &nbsp; <i class="now-ui-icons arrows-1_cloud-upload-94"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section" style="box-shadow:0px 0px 60px rgba(0,0,0,0.1);">
+            <div class="space-50"></div>
+            <div class="container text-center">
+                <div class="row justify-content-md-center" id="screenshots">
+                    <div class="col-md-12" style="margin-bottom:15px;margin-top:25px;">
+                        <h5 class="description" style="text-align:center;color:#222;font-weight:300;"><strong>Step 2:</strong> Login.</h5>
+                        <p>Sign in by going to <strong>www.yourwebsite.com/admin/login</strong></p>
+                        <p>The default account is <strong>admin@admin.com</strong> and the password is <strong>password</strong>.</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/pages/getting-started/rKbpwckdgZlJc3VBozhM6uB7kTt48nSUtF8tDOPV.jpeg" alt="Deploying to Heroku" class="img-raised">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section" style="box-shadow:0px 0px 60px rgba(0,0,0,0.1);">
+            <div class="space-50"></div>
+            <div class="container text-center">
+                <div class="row justify-content-md-center" id="screenshots">
+                    <div class="col-md-12" style="margin-bottom:15px;margin-top:25px;">
+                        <h5 class="description" style="text-align:center;color:#222;font-weight:300;"><strong>Step 3:</strong> Start creating.</h5>
+                    </div>
+                    <div class="col-lg-4">
+                        <img src="https://psychoanalytics.s3-us-west-1.amazonaws.com/pages/getting-started/py90ClPUQV06veHJtW90pglJvOIJmBgyAFD7Iuh6.jpeg" alt="Deploying to Heroku" class="img-raised">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section bg-gradient-blue" style="box-shadow:0px 0px 60px rgba(0,0,0,0.1);color:#fff;">
+            <div class="container text-center">
+                <div class="row justify-content-md-center">
+                    <div class="col-md-12 col-lg-8">
+                        <h2 class="title">Need more help?</h2>
+                        <h5 class="description" style="color:#fff;font-weight:300;margin-bottom:35px;">Check out our help center for tutorials, documentation, and more.</h5>
+                        <a href="/help" class="btn bg-gradient-orange btn-lg" onclick="mixpanel.track('cta');">Show me the docs &nbsp;<i class="now-ui-icons education_paper"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
