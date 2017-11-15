f9ae2adb831d4d727fd42cb4e7c5d4f32227958d
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-11-15 04:07:05 -0800

    Dynamic title

--- pages/articles/body.blade.php
+++ pages/articles/body.blade.php
@@ -6,7 +6,7 @@
         </div>
         <div class="container">
             <div class="content-center" style="top:200px !important;">
-                    <h1 class="title text-center" style="padding-bottom: 25px;border-bottom: #fff solid 5px;margin:25px;">Articles</h1>
+                    <h1 class="title text-center" style="padding-bottom: 25px;border-bottom: #fff solid 5px;margin:25px;">{{ $page->title }}</h1>
             </div>
         </div>
     </div>
