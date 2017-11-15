35735b2ba418f2c7408e110c5dcce0885bd23aa7
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-11-15 13:08:42 -0800

    Layout

--- pages/articles/scripts.blade.php
+++ pages/articles/scripts.blade.php
@@ -1,7 +1,7 @@
 <script>
     Vue.component('todo-item', {
         props: ['todo'],
-        template: '<div class="col-sm-4"><a class="card-link" v-bind:href="todo.slug"><div class="card" style="height:100%;"><img class="card-img-top" v-bind:src="todo.image"><div class="card-body"><h4 class="card-title" align="center">@{{ todo.title }}</h4><p class="card-text">@{{ todo.meta_description }}</p></div><div class="card-footer" align="center">Read More</div></div></a></div>'
+        template: '<div class="col-sm-4" style="margin-top:25px;"><a class="card-link" v-bind:href="todo.slug"><div class="card" style="height:100%;"><img class="card-img-top" v-bind:src="todo.image"><div class="card-body"><h4 class="card-title" align="center">@{{ todo.title }}</h4><p class="card-text">@{{ todo.meta_description }}</p></div><div class="card-footer" align="center">Read More</div></div></a></div>'
     });
     var app7 = new Vue({
         el: '#articles',
