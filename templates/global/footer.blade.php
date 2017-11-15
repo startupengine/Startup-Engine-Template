aac668774032a9dcddca715451850e643a637a78
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-11-14 22:47:00 -0800

    Removing footer

--- templates/global/footer.blade.php
+++ templates/global/footer.blade.php
@@ -1,3 +1,17 @@
 <!-- Footer -->
 <!-- End Footer -->
+<div class="section bg-gradient-orange" style="box-shadow:0px 0px 60px rgba(0,0,0,0.1);color:#fff;width:100%;">
+    <div class="container text-center">
+        <div class="row justify-content-md-center">
+            <div class="col-md-12 col-lg-8">
+                <h2 class="title">Ready to get started?</h2>
+                <h5 class="description" style="color:#fff;font-weight:300;margin-bottom:35px;">Get up and running in 5
+                    minutes or less.</h5>
+                <a href="/get-started" class="btn btn-neutral btn-lg" onclick="mixpanel.track('cta');"><i
+                            class="now-ui-icons arrows-1_share-66"></i> Get Started</a>
+                <a href="/get-started" class="btn btn-neutral btn-simple btn-lg">View the Docs</a>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
