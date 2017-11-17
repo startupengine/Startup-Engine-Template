bf1068530e63efbb9e4e273eff65f1119976370d
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-11-16 23:37:06 -0800

    CSS

--- pages/articles/css.blade.php
+++ pages/articles/css.blade.php
@@ -20,7 +20,7 @@
         border-radius: 5px;
         background: #fff;
         box-shadow: 0px -60px 60px rgba(0,0,0,0.25);
-        padding-top:50px;
+        padding-top:10px;
         padding-right: 50px;
     }
 </style>
