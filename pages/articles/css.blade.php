d2f42ec62b3cc60fdeb3eea4d16fb034efc83330
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-11-17 01:09:38 -0800

    CSS

--- pages/articles/css.blade.php
+++ pages/articles/css.blade.php
@@ -17,10 +17,12 @@
         background: linear-gradient(to top right, #ff35a4, #350090);
     }
     #articles {
+        /*
         border-radius: 5px;
         background: #fff;
         box-shadow: 0px -30px 60px rgba(0,0,0,0.2);
         padding:0px 30px 30px 0px;
+        */
     }
     @media only screen and (max-width : 1024px) {
         #articles{
