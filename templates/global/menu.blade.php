0e74015625aaf9fe8207bcdfb6157d868bcf5998
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-11-16 23:23:24 -0800

    Dropdown

--- templates/global/menu.blade.php
+++ templates/global/menu.blade.php
@@ -22,7 +22,7 @@
                <?php echo setting('site.menu'); ?>
                 <?php if(\Auth::user() !== null && \Auth::user()->role()->name == 'admin'){  ?>
                 <li class="nav-item">
-
+                    <div class="btn-group">
                     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Account
                     </a>
@@ -30,6 +30,7 @@
                         <button class="dropdown-item" type="button" onclick="location.href='/';"><i class="now-ui-icons arrows-1_share-66"></i>&nbsp; Site</button>
                         <button class="dropdown-item" type="button" onclick="location.href='/app/profile';"><i class="now-ui-icons users_circle-08"></i>&nbsp; Profile</button>
                         <button class="dropdown-item" type="button" onclick="location.href='/logout';"><i class="now-ui-icons ui-1_lock-circle-open"></i>&nbsp; Sign Out</button>
+                    </div>
                     </div>
 
                 </li>
