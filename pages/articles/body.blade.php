e9524df43c96f9d94a1af6b63122ea4be0253d36
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-12-15 21:39:05 -0800

    Revert "Renaming component"
    This reverts commit 98a7e43f483477ddc8a1c175025fc9eec8910d43.

--- pages/articles/body.blade.php
+++ pages/articles/body.blade.php
@@ -18,12 +18,12 @@
             <div class="row">
                 <div class="col-lg-12 col-md-12">
                     <div id="articles" class="card-deck">
-                        <articles
+                        <article
                                 v-for="item in items"
                                 v-bind:todo="item"
                                 v-bind:key="item.id"
                                 v-bind:href="item.slug">
-                        </articles>
+                        </article>
                     </div>
                 </div>
             </div>
