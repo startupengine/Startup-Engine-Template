ce9cc34802b7a493ea571283286466ca0eddf8f5
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-12-17 17:26:26 -0800

    CSS

--- templates/post/css.blade.php
+++ templates/post/css.blade.php
@@ -6,7 +6,7 @@
     #content > div:first-of-type {
         margin-top:0px !important;
     }
-    #content > div:first-of-type > h1 {
+    #content > h1 {
         text-align:center;
     }
 </style>
