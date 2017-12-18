b5882c4e81db3db5955f703bef9366c33be5c05b
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-12-18 03:32:29 -0800

    CSS

--- templates/post/css.blade.php
+++ templates/post/css.blade.php
@@ -9,4 +9,8 @@
     #content h1, #content h2, #content h3, #content h4, #content h5, #content h6 {
         text-align:center !important;
     }
+    .btn.btn-simple {
+        color:#fff !important;
+        border-color:#fff !important;
+    }
 </style>
