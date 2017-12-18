8d3a9c55bca2fca8badc457fbe9e0209e7c12bfd
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-12-17 23:44:27 -0800

    Added about section

--- pages/articles/body.blade.php
+++ pages/articles/body.blade.php
@@ -19,12 +19,26 @@
             <div class="row">
                 <div class="col-lg-12 col-md-12">
                     <div id="articles" class="card-deck">
+                        <div class="@if($page->content()->about->headline !== null) col-md-8 @else col-md-12 @endif">
                         <todo-item
                                 v-for="item in items"
                                 v-bind:article="item"
                                 v-bind:key="item.id"
                                 v-bind:href="item.slug">
                         </todo-item>
+                        </div>
+                        @if($page->content()->about->headline !== null)
+                        <div class="col-md-4">
+                            <div class="card" style="margin-top:25px;">
+                                @if($page->content()->about->headline !== null)
+                                    <p><strong>{{ $page->content()->about->headline }}</strong></p>
+                                @endif
+                                @if($page->content()->about->headline !== null)
+                                    <p><strong>{{ $page->content()->about->headline }}</strong></p>
+                                @endif
+                            </div>
+                        </div>
+                        @endif
                     </div>
                 </div>
             </div>
