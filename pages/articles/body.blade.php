1a06c8b604d43cf6ffaa70aeea56874e1c79477a
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-12-20 16:07:40 -0800

    Layout

--- pages/articles/body.blade.php
+++ pages/articles/body.blade.php
@@ -20,41 +20,39 @@
 <div class="wrapper">
     <div class="section" style="padding-top:50px;margin-top:-100px !important;background:none;">
         <div class="container">
-            <div class="row">
-                <div id="articles" class="card-deck">
-                    <div class="row">
-                        <div class="@if($page->content()->about->headline !== null) col-md-8 @else col-md-12 @endif">
-                            <todo-item
-                                    v-for="item in items"
-                                    v-bind:article="item"
-                                    v-bind:key="item.id"
-                                    v-bind:href="item.slug">
-                            </todo-item>
-                        </div>
-                        @if($page->content()->about->headline !== null)
-                            <div class="col-md-4">
-                                <div class="col-md-12">
-                                    <div class="card" style="margin-top:25px;">
-                                        @if($page->content()->about->headline !==null OR $page->content()->about->description !== null)
-                                            <div class="card-body">
-                                                @if($page->content()->about->headline !== null)
-                                                    <p><strong>{{ $page->content()->about->headline }}</strong></p>
-                                                @endif
-                                                @if($page->content()->about->description !== null)
-                                                    <p>{{ $page->content()->about->description }}</p>
-                                                @endif
-                                            </div>
-                                        @endif
-                                    </div>
-                                    @if($page->content()->sidebar->html!== null)
-                                        @if($page->content()->sidebar->html !== null)
-                                            <?php echo $page->markdown($page->content()->sidebar->html); ?>
-                                        @endif
+            <div id="articles" class="card-deck">
+                <div class="row">
+                    <div class="@if($page->content()->about->headline !== null) col-md-8 @else col-md-12 @endif">
+                        <todo-item
+                                v-for="item in items"
+                                v-bind:article="item"
+                                v-bind:key="item.id"
+                                v-bind:href="item.slug">
+                        </todo-item>
+                    </div>
+                    @if($page->content()->about->headline !== null)
+                        <div class="col-md-4">
+                            <div class="col-md-12">
+                                <div class="card" style="margin-top:25px;">
+                                    @if($page->content()->about->headline !==null OR $page->content()->about->description !== null)
+                                        <div class="card-body">
+                                            @if($page->content()->about->headline !== null)
+                                                <p><strong>{{ $page->content()->about->headline }}</strong></p>
+                                            @endif
+                                            @if($page->content()->about->description !== null)
+                                                <p>{{ $page->content()->about->description }}</p>
+                                            @endif
+                                        </div>
                                     @endif
                                 </div>
+                                @if($page->content()->sidebar->html!== null)
+                                    @if($page->content()->sidebar->html !== null)
+                                        <?php echo $page->markdown($page->content()->sidebar->html); ?>
+                                    @endif
+                                @endif
                             </div>
-                        @endif
-                    </div>
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
