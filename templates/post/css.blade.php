4721fd628bc33e8386f7a9ed8aa8d6901fa15a31
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-12-22 03:06:53 -0800

    CSS

--- templates/post/css.blade.php
+++ templates/post/css.blade.php
@@ -27,8 +27,8 @@
         padding:0px !important;
     }
 
-    #content p {
-        padding:25px;
+    #content p, #content h1, #content h2, #content h3, #content h4, #content h5, #content h6 {
+        padding:25px !important;
     }
 
     #content > .section {
