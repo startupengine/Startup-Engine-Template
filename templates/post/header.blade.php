5d66c6d5c29665636c6338f8664bb980f94a1188
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-12-22 03:14:36 -0800

    CSS

--- templates/post/header.blade.php
+++ templates/post/header.blade.php
@@ -28,7 +28,7 @@
 <div class="section" style="padding-top:25px;margin-top:-75px !important;background:none;">
     <div class="container" id="more">
         <div class="row">
-            <div class="col-lg-12 col-md-12">
+            <div>
                 <div id="articles" >
                     <div id="content-row">
 
