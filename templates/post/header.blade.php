a238cc054b10d46c4bfe489f469099db03a6ede6
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-12-22 01:48:53 -0800

    CSS

--- templates/post/header.blade.php
+++ templates/post/header.blade.php
@@ -30,7 +30,7 @@
         <div class="row">
             <div class="col-lg-12 col-md-12">
                 <div id="articles" class="card-deck">
-                    <div class="col-md-12 row row-eq-height">
+                    <div class="row row-eq-height">
 
                         <div class="card" style="box-shadow:0px -30px 60px rgba(0,0,0,0.2);">
                             <?php if($post->content() !== null && $post->content()->body->image !== null){ ?>
