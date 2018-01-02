5b91227620b418a344c9ede91d4d914fd0463524
huntercarter <jhc@Jamess-MacBook-Pro.local>
2018-01-02 02:45:53 -0800

    Simplifying menu logic

--- templates/global/menu.blade.php
+++ templates/global/menu.blade.php
@@ -2,10 +2,13 @@
 <nav class="navbar navbar-expand-lg bg-white fixed-top navbar-transparent " color-on-scroll="100">
     <div class="container">
         <div class="navbar-translate">
-            <a class="navbar-brand" href="/" rel="tooltip" title="{{ setting('site.description') }}" data-placement="bottom">
-                <?php if(setting('site.logo') !== null) { ?><img src="{{ setting('site.logo') }}" alt="Logo Icon" style="max-width:40px;"> <?php } ?>{{ setting('site.name') }}
+            <a class="navbar-brand" href="/" rel="tooltip" title="{{ setting('site.description') }}"
+               data-placement="bottom">
+                <?php if(setting('site.logo') !== null) { ?><img src="{{ setting('site.logo') }}" alt="Logo Icon"
+                                                                 style="max-width:40px;"> <?php } ?>{{ setting('site.name') }}
             </a>
-            <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
+                    aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-bar bar1"></span>
                 <span class="navbar-toggler-bar bar2"></span>
                 <span class="navbar-toggler-bar bar3"></span>
@@ -14,26 +17,39 @@
         <div class="collapse navbar-collapse justify-content-end" id="navigation">
             <div id="mobile-nav-brand" align="center" style="
     margin-top: 25px;
-"> <a class="navbar-brand" href="/" style="margin:0px;text-align:center;">
-                    <?php if(setting('site.logo') !== null) { ?><img src="{{ setting('site.logo') }}" alt="Logo Icon" style="max-width:40px;"> <?php } ?>{{ setting('site.name') }}</span>
+"><a class="navbar-brand" href="/" style="margin:0px;text-align:center;">
+                    <?php if(setting('site.logo') !== null) { ?><img src="{{ setting('site.logo') }}" alt="Logo Icon"
+                                                                     style="max-width:40px;"> <?php } ?>{{ setting('site.name') }}</span>
                 </a>
             </div>
             <ul class="navbar-nav">
-               <?php echo setting('site.menu'); ?>
-                <?php if(\Auth::user() !== null && \Auth::user()->role()->name == 'admin'){  ?>
+                <?php echo setting('site.menu'); ?>
+                <?php if(\Auth::user() !== null) {  ?>
                 <li class="nav-item">
                     <div class="btn-group hiddenOnMobile">
-                        <a href="#" class="nav-link dropdown-toggle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a href="#" class="nav-link dropdown-toggle " data-toggle="dropdown" aria-haspopup="true"
+                           aria-expanded="false">
                             Account
                         </a>
                         <div class="dropdown-menu dropdown-menu-right ">
-                            <button class="dropdown-item" type="button" onclick="location.href='/app';"><i class="now-ui-icons arrows-1_share-66"></i>&nbsp; App</button>
-                            <button class="dropdown-item" type="button" onclick="location.href='/app/profile';"><i class="now-ui-icons users_circle-08"></i>&nbsp; Profile</button>
-                            <button class="dropdown-item" type="button" onclick="location.href='/logout';"><i class="now-ui-icons ui-1_lock-circle-open"></i>&nbsp; Sign Out</button>
+                            <button class="dropdown-item" type="button" onclick="location.href='/app/profile';"><i
+                                        class="now-ui-icons users_circle-08"></i>&nbsp; Profile
+                            </button>
+                            <button class="dropdown-item" type="button" onclick="location.href='/logout';"><i
+                                        class="now-ui-icons ui-1_lock-circle-open"></i>&nbsp; Sign Out
+                            </button>
                         </div>
                     </div>
-                    <a href="/app" class="nav-link hiddenOnDesktop">App &nbsp;<i class="fas fa-sign-in-alt"></i></a>
-                    <a href="/logout" class="nav-link hiddenOnDesktop">Sign Out &nbsp;<i class="fas fa-sign-out-alt"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a href="/app/profile" class="nav-link hiddenOnDesktop">
+                        <i class="now-ui-icons users_circle-08"></i> &nbsp;Profile
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="/logout" class="nav-link hiddenOnDesktop">
+                        <i class="now-ui-icons ui-1_lock-circle-open"></i> &nbsp;Sign Out
+                    </a>
                 </li>
                 <?php } ?>
             </ul>
