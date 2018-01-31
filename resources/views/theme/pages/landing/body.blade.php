dc700d30d33fa15431936bc5849bd1dc9794ffd0
huntercarter <jhc@Jamess-MacBook-Pro.local>
2018-01-31 15:59:31 -0800

    Adding conditionals for each field

--- resources/views/theme/pages/landing/body.blade.php
+++ resources/views/theme/pages/landing/body.blade.php
@@ -126,9 +126,9 @@
         <div class="container">
             <div class="row align-items-center text-center">
                 <div class="col-lg-12 text-center">
-                    <h2>Read to get started?</h2>
-                    <a href="https://app.psychoanalytics.io" class="btn btn-lg btn-primary btn-round bg-gradient-orange grow">Create Your
-                        Account &nbsp; <i class="fa fa-caret-right"></i>
+                    @if(isset($page->content()->call_to_action->headline))<h2>{{ $page->content()->call_to_action->headline }}</h2>@endif
+                    @if(isset($page->content()->call_to_action->message))<h5 class="description">{{ $page->content()->call_to_action->message }}</h5>@endif
+                    @if(isset($page->content()->call_to_action->button) && isset($page->content()->call_to_action->link))<a href="{{ $page->content()->call_to_action->link }}" class="btn btn-lg btn-primary btn-round bg-gradient-orange grow">{{ $page->content()->call_to_action->button }}@endif
                     </a>
                 </div>
             </div>
