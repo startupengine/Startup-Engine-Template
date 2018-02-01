84127a3e19080ddeff2ea9870bcb5a9b32b770f5
huntercarter <jhc@Jamess-MacBook-Pro.local>
2018-01-31 20:46:32 -0800

    Adding element id

--- resources/views/theme/pages/help/header.blade.php
+++ resources/views/theme/pages/help/header.blade.php
@@ -1,7 +1,7 @@
 <body class="index-page sidebar-collapse">
 <div id="startup_engine_nav_container"></div>
 <div class="wrapper">
-    <div class="page-header page-header-small clear-filter" filter-color="black">
+    <div id="page-header" class="page-header page-header-small clear-filter" filter-color="black">
         <div class="page-header-image" data-parallax="true" style="@if($page->content()->heading->background == null)background-color:#3a92f1;@else background-image:url({{ $page->content()->heading->background }}); @endif">
         </div>
         <div class="container">
