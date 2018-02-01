0a75dd5df42bd6fce1aa589b5f57acfe60e0cde8
huntercarter <jhc@Jamess-MacBook-Pro.local>
2018-01-31 16:08:35 -0800

    Removing unnecessary parenthesis

--- resources/views/theme/pages/landing/body.blade.php
+++ resources/views/theme/pages/landing/body.blade.php
@@ -128,7 +128,7 @@
                 <div class="col-lg-12 text-center">
                     @if(isset($page->content()->call_to_action->headline))<h2>{!! $page->markdown($page->content()->call_to_action->headline) !!}</h2>@endif
                     @if(isset($page->content()->call_to_action->message))<h5 class="description">{!! $page->markdown($page->content()->call_to_action->message) !!}</h5>@endif
-                    @if(isset($page->content()->call_to_action->button) && isset($page->content()->call_to_action->link))<a href="{{ $page->content()->call_to_action->link) }}" class="btn btn-lg btn-primary btn-round bg-gradient-orange grow">{!! $page->markdown($page->content()->call_to_action->button) !!}</a>@endif
+                    @if(isset($page->content()->call_to_action->button) && isset($page->content()->call_to_action->link))<a href="{{ $page->content()->call_to_action->link }}" class="btn btn-lg btn-primary btn-round bg-gradient-orange grow">{!! $page->markdown($page->content()->call_to_action->button) !!}</a>@endif
                 </div>
             </div>
         </div>
