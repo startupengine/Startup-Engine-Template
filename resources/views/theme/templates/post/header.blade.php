b9474fe2cfd67852633ecebb5bf278c591f35f8c
huntercarter <jhc@Jamess-MacBook-Pro.local>
2018-02-01 21:06:52 -0800

    Display primary tag above post header

--- resources/views/theme/templates/post/header.blade.php
+++ resources/views/theme/templates/post/header.blade.php
@@ -1,4 +1,5 @@
-<body class="index-page sidebar-collapse" id="articles-index" style="background: linear-gradient(rgb(235, 242, 255), rgb(255, 255, 255));">
+<body class="index-page sidebar-collapse" id="articles-index"
+      style="background: linear-gradient(rgb(235, 242, 255), rgb(255, 255, 255));">
 
 @if(View::exists('theme.templates.global.menu'))
     @include('theme.templates.global.menu')
@@ -6,6 +7,33 @@
 
 <div id="startup_engine_nav_container"></div>
 <div class="wrapper" style="background:#000;">
+
+    @if(count($post->tagNames()) > 0 )
+    <div class="page-header  page-header-small " style="
+    height: 200px !important;
+    max-height: 200px !important;
+    min-height: 200px !important;
+    border-bottom: 1px solid rgba(255,255,255,0.25);">
+        <div class="container">
+            <div class="content-center">
+                <div style="padding-top:75px;">
+                    <div align="center">
+                        <h5 id="title">
+                            <?php $tagCount = 1; ?>
+                                @foreach($post->tagNames() as $tagName)
+                                    @if($tagCount <= 1)
+                                        <?php $firstTagName = $tagName; ?>
+                                    @endif
+                                @endfor
+                            <span style="opacity:0.5;">Category - </span><a href="/content/tag/{{ strtolower($firstTagName) }}" style="color:#fff !important;text-decoration:none !important;">{{ ucwords($firstTagName) }}</a>
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
+
     <div class="page-header @if(isset($post->content()->heading->header_size) && $post->content()->heading->header_size == 'small') page-header-small @endif">
         <div class="page-header-image" <?php if ($post->content()->heading->background !== null) {
             echo "style=\"background-image:url('" . $post->content()->heading->background . "'); background-size:cover;z-index: 0;opacity: 0.3; \"";
@@ -17,15 +45,18 @@
                     <div align="center">
                         <h1 id="title">{{ $post->content()->heading->headline }}</h1>
                         @if($post->content()->heading->header_size == 'large')
-                            <h2 class="excerpt" style="margin-bottom:25px;">{{ $post->content()->heading->excerpt }}</h2>
+                            <h2 class="excerpt"
+                                style="margin-bottom:25px;">{{ $post->content()->heading->excerpt }}</h2>
                         @endif
                         @if(count($post->tagNames()) > 0)
                             <div style="margin-top:25px;margin-bottom: 25px;">
                                 <?php $tagCount = 1; ?>
-                                <div class="badge hiddenOnMobile" style="border-color:rgba(255,255,255,0.33);">Tags</div>
+                                <div class="badge hiddenOnMobile" style="border-color:rgba(255,255,255,0.33);">Tags
+                                </div>
                                 @foreach($post->tagNames() as $tagName)
                                     @if($tagCount <= 3)
-                                        <a href="/content/tag/{{ strtolower($tagName) }}" class="badge">{{ $tagName }}</a>
+                                        <a href="/content/tag/{{ strtolower($tagName) }}"
+                                           class="badge">{{ $tagName }}</a>
                                     @endif
                                     <?php $tagCount = $tagCount + 1; ?>
                                 @endforeach
@@ -42,9 +73,9 @@
                             </h6>
                         @endif
                         @if($post->content()->heading->button !== null)
-                        <a class="btn btn-lg btn-round btn-simple" id="engage" href="javascript:void(0)"
-                           onclick="$('#more').ScrollTo();">@if($post->content()->heading->button !== null) {{ $post->content()->heading->button }} @else
-                                Read Article @endif</a>
+                            <a class="btn btn-lg btn-round btn-simple" id="engage" href="javascript:void(0)"
+                               onclick="$('#more').ScrollTo();">@if($post->content()->heading->button !== null) {{ $post->content()->heading->button }} @else
+                                    Read Article @endif</a>
                         @endif
                     </div>
                 </div>
@@ -52,14 +83,17 @@
         </div>
     </div>
 </div>
-<div class="section" style="padding-top:25px;@if($post->content()->heading->header_size == "small") margin-top:-75px !important; @else margin-top:-150px !important; @endif background:none;z-index:1 !important;">
+<div class="section"
+     style="padding-top:25px;@if($post->content()->heading->header_size == "small") margin-top:-75px !important; @else margin-top:-150px !important; @endif background:none;z-index:1 !important;">
     <div class="container" id="more">
         <div class="row">
             <div style="width:100%;">
                 <div id="articles">
                     <div id="content-row">
 
-                        <div class="card" style="text-align:center;box-shadow:0px -30px 30px rgba(0,0,100,0.1);background:linear-gradient(180deg, #fff 90%, #fff0 100%) !important;" id="contentBody">
+                        <div class="card"
+                             style="text-align:center;box-shadow:0px -30px 30px rgba(0,0,100,0.1);background:linear-gradient(180deg, #fff 90%, #fff0 100%) !important;"
+                             id="contentBody">
                             <?php if($post->content() !== null && $post->content()->body->image !== null && $post->content()->body->video == null){ ?>
                             <div id="featuredImage"
                                  style="background:url('{{ $post->content()->body->image }}');"></div>
@@ -82,9 +116,9 @@
                                    @if($video !== null) data-{{ $videotype }}-id="{{ $video }}" @endif
                                    @if($post->content()->body->image !== null) poster="{{ $post->content()->body->image }}" @endif >
 
-                                    @if($videotype !== "youtube" && $videotype !== "vimeo")
-                                        <source type="video/mp4" src="{{ $post->content()->body->video }}" />
-                                    @endif
+                                @if($videotype !== "youtube" && $videotype !== "vimeo")
+                                    <source type="video/mp4" src="{{ $post->content()->body->video }}"/>
+                                @endif
 
                             </video>
                             <?php } ?>
