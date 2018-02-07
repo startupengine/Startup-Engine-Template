236de1b44687d829591f4bfaa2f36848df74409e
huntercarter <jhc@Jamess-MacBook-Pro.local>
2018-02-07 10:57:41 -0800

    Cleanup

--- resources/views/theme/pages/articles/scripts.blade.php
+++ resources/views/theme/pages/articles/scripts.blade.php
@@ -1,3 +1,10 @@
+<?php
+if(isset($tag)) {
+    $tagContent = \App\Tag::where('slug', '=', $tag)->first();
+} else {
+    $tagContent = null;
+} ?>
+
 <script>
     var getUrlParameter = function getUrlParameter(sParam) {
         var sPageURL = decodeURIComponent(window.location.search.substring(1)),
@@ -23,18 +30,18 @@
         pageSize = 30;
     }
 
-    <?php if(isset($tag)) { ?>
-        var tags = '&tag[]=<?php echo $tag; ?>';
-    <?php } elseif(isset($page->content()->settings->required_tags)) {
-        $required_tags = explode(",", $page->content()->settings->required_tags);
-        $tagString = '';
-        foreach($required_tags as $required_tag) {
-            $tagString = $tagString."&tag[]=$required_tag";
-        }
-        echo "var tags = '$tagString';";
-    } else { ?>
-        var tags = '';
-    <?php } ?>
+        <?php if(isset($tag)) { ?>
+    var tags = '&tag[]=<?php echo $tag; ?>';
+        <?php } elseif(isset($page->content()->settings->required_tags)) {
+            $required_tags = explode(",", $page->content()->settings->required_tags);
+            $tagString = '';
+            foreach($required_tags as $required_tag) {
+                $tagString = $tagString."&tag[]=$required_tag";
+            }
+            echo "var tags = '$tagString';";
+        } else { ?>
+    var tags = '';
+        <?php } ?>
 
         <?php if (isset($page->content()->settings->excluded_tags)) {
             $excluded_tags = explode(",", $page->content()->settings->excluded_tags);
@@ -47,13 +54,32 @@
             echo "var excludeTags = '';";
         }?>
 
-        var queryPath = '/api/content/items?page[number]='+ pageNumber +'&page[size]='+ pageSize + tags + excludeTags;
+    var queryPath = '/api/content/items?page[number]='+ pageNumber +'&page[size]='+ pageSize + tags + excludeTags;
 
     Vue.component('todo-item', {
         props: ['article'],
         template:
-        '<div class="col-md-6" style="margin-top:25px;">' +
+        @if(isset($tag) && isset($tagContent) && $tagContent->content()->page->format == "list")
+        '<div class="col-md-12" style="margin-top:25px;">' +
         '<a class="card-link" v-bind:href="article.slug" onclick="window.location.href = $(this).attr(\'href\');">' +
+        '<div class="card list-item" style="height:100%;">' +
+        '<div v-if="article.content.meta && article.content.meta.featured == \'on\' && article.content.meta.message == null" class="card-header featured" ><i class="fas fa-star" ></i>  Featured</div>' +
+        '<div v-if="article.content.meta && article.content.meta.featured == \'on\' && article.content.meta.message !== null" class="card-header featured" >@{{article.content.meta.message}}</div>' +
+        '<div v-if="article.content.heading.background !== null && article.content.meta.featured == \'on\'" :style=\'{ backgroundImage: `url(${article.content.heading.background})` }\' class="image-container"></div>' +
+        '<div v-else-if="article.content.heading.background !== null && article.content.meta.message !== null" :style=\'{ backgroundImage: `url(${article.content.heading.background})` }\' class="image-container"></div>' +
+        '<div v-else :style=\'{ backgroundImage: `url(${article.content.heading.background})` }\' class="image-container"></div>' +
+        '<div class="card-body">' +
+        '<h4 class="card-title" align="center">@{{ article.content.heading.headline }}</h4>' +
+        '<p v-if="article.content.heading.excerpt !== null">@{{article.content.heading.excerpt}}</p>' +
+        '<p></p>'+
+        '<div class="btn btn-link btn-lg btn-block" style="padding:0px 25px 0px 0px !important;"><span style="border-radius:25px;border:1px solid #eee; padding:5px 20px;">Continue Reading  <i class="fas fa-arrow-right fa-xs"></i></span></div>' +
+        '</div>' +
+        '</div>' +
+        '</a>' +
+        '</div>'
+        @else
+        '<div class="col-md-6" style="margin-top:25px;">' +
+            '<a class="card-link" v-bind:href="article.slug" onclick="window.location.href = $(this).attr(\'href\');">' +
         '<div class="card" style="height:100%;">' +
         '<div v-if="article.content.meta && article.content.meta.featured == \'on\' && article.content.meta.message == null" class="card-header featured" ><i class="fas fa-star" ></i>  Featured</div>' +
         '<div v-if="article.content.meta && article.content.meta.featured == \'on\' && article.content.meta.message !== null" class="card-header featured" >@{{article.content.meta.message}}</div>' +
@@ -69,39 +95,33 @@
         '</div>' +
         '</a>' +
         '</div>'
+        @endif
     });
 
-    <?php if(isset($tag)) { $element = "#page"; } else { $element = "#articles"; } ?>
+        <?php if(isset($tag)) { $element = "#page"; } else { $element = "#articles"; } ?>
 
     var app = new Vue({
-        el: '<?php echo $element; ?>',
-        data: {
-            items: null,
-            resource_url: queryPath
-        },
-        components: {
-            VPaginator: VuePaginator
-        },
-        methods: {
-            updateResource(data){
-                this.items = data
+            el: '<?php echo $element; ?>',
+            data: {
+                items: null,
+                resource_url: queryPath
+            },
+            components: {
+                VPaginator: VuePaginator
+            },
+            methods: {
+                updateResource(data){
+                    this.items = data
+                }
+            },
+            created: function getContent() {
+                var _this = this;
+                $.getJSON(queryPath, function (json) {
+                    _this.items = json.data;
+                });
             }
-        },
-        created: function getContent() {
-            var _this = this;
-            $.getJSON(queryPath, function (json) {
-                _this.items = json.data;
-            });
-        }
-    });
+        });
 </script>
-<?php
-if(isset($tag)) {
-    $tagContent = \App\Tag::where('slug', '=', $tag)->first();
-} else {
-    $tagContent = null;
-} ?>
-
 @if($page->content()->code->scripts !== null)
     {!! $page->content()->code->scripts  !!}
 @endif
--- resources/views/theme/templates/global/header.blade.php
+++ resources/views/theme/templates/global/header.blade.php
@@ -5,7 +5,7 @@
 
 <!--  Core JS -->
 <script src="/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
-<script src="https://unpkg.com/vue"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.5.13/dist/vue.min.js"></script>
 <script src="/js/core/popper.min.js" type="text/javascript"></script>
 <script src="/js/core/bootstrap.min.js" type="text/javascript"></script>
 
